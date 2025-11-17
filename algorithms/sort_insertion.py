43e2a726e203672ea8d716107fbe892e12e1cc12
Guido Vazquez <guidoezequiel28@gmail.com>
2025-11-16 23:18:16 -0300

    Update sort_insertion.py

--- algorithms/sort_insertion.py
+++ algorithms/sort_insertion.py
@@ -13,9 +13,23 @@ def init(vals):
     j = None
 
 def step():
+    global items, n, i, j 
+
+    # Si ya terminamos
+    if i >= n:
+        return {"a": None, "b": None, "swap": False, "done": True}
+
+    # Si j es None, iniciamos el desplazamiento del elemento en i
+    if j == None: 
+        j = i 
+        return {"a": j-1 if j > 0 else None, "b": j, "swap": False, "done": False}
+
+    
+
+    
     # TODO:
     # - Si i >= n: devolver {"done": True}.
     # - Si j es None: empezar desplazamiento para el items[i] (p.ej., j = i) y devolver un highlight sin swap.
     # - Mientras j > 0 y items[j-1] > items[j]: hacer UN swap adyacente (j-1, j) y devolverlo con swap=True.
     # - Si ya no hay que desplazar: avanzar i y setear j=None.
-    return {"done": True}
+   
