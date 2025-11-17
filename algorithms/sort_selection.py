38bf99bc03eff2b47b00fcb1e5516ef7e84e3ecb
Guido Vazquez <guidoezequiel28@gmail.com>
2025-11-16 23:09:27 -0300

    Update sort_selection.py

--- algorithms/sort_selection.py
+++ algorithms/sort_selection.py
@@ -19,35 +19,44 @@ def init(vals):
 def step():
     global items, n, i, j, min_idx, fase
 
-    #Si terminamos 
+    # Si terminamos 
     if i >= n - 1:
-        return {done: True}
-    #Fase "buscar" el minimo
-    if fase == "buscar":
-        #Mientras j este dentro de la lista, seguimos comparando
-        if j < n: 
-            a = min_idx #indice del minimo actual
-            b = j #indice que estamos comparando ahora
-            
-            #Comparacion 
-            if items[j]<items[min_idx]:
-                min_idx = j #nuevo minimo encontrado
-            j = j + 1  #avanzar a la siguiente vuelta
-            return { "a": a, "b": b, "swap": False, "done": False}
-        #si terminamos de desplazarlos, pasamos a la fase swap
-        fase = "swap"
-    # Fase de hacer el swap
-    
+        return {"done": True} 
         
-    
-    
-    
-        for j in range (i+1, len(lista)): 
-            if min_idx > lista[j]:
-                min_idx =j
-                
-    aux = lista[j]
-    lista[i] = lista[min_idx]
-    lista[min_idx] = aux 
-    
-    return {"done": True}
+    # Fase "buscar" (el minimo)
+    if fase == "buscar":        
+        if j < n:           # Mientras j este dentro de la lista, seguimos comparando
+            a = min_idx     # indice del minimo actual
+            b = j           # indice que estamos comparando ahora
+            
+            # Comparacion 
+            if items[j]<items[min_idx]:
+                min_idx = j     # Nuevo minimo encontrado
+            j = j + 1           #Avanzar a la siguiente vuelta
+            
+            return { "a": a, "b": b, "swap": False, "done": False}
+            
+        # Si terminamos de desplazarlos, pasamos a la fase swap
+        fase = "swap"
+        
+    # Fase de hacer el swap
+    if fase == "swap":
+        a = i
+        b = min_idx
+
+        hubo_swap = False
+
+        if min_idx != i:
+            # Hacemos el swap en items
+            aux = items[i]
+            items[i] = items[min_idx]
+            items[min_idx] = aux
+            hubo_swap = True
+
+        # Avanzar a la proxima pasada
+        i = i + 1
+        j = i + 1 
+        min_idx = i 
+        fase = "buscar"
+
+        return {"a": a, "b": b, "swap": hubo_swap, "done": False}
